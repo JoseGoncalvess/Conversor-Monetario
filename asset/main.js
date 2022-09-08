aee1fbbd157bb610abdc30a56b69e1b1c444e183
José Gonçalves <joseinacio774@gmail.com>
2022-09-08 08:43:07 -0300

    add img

---
+++ asset/main.js
@@ -0,0 +1,10 @@
+//[Cotção do dia 08/09/2022]
+let dolar = 5.23
+let euro = 5.23
+let bitcoin = 101.619
+
+//[Variaveis de ambiente DOM]
+
+//[Funções]
+function converter() {}
+function limpar() {}
